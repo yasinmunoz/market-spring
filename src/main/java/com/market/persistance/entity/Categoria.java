d22566fbc224052a9b4d85bd30566ef439428f5a
Yasín Muñoz <yasinmunoz@hotmail.com>
2022-04-15 02:21:13 +0200

    first commit

---
+++ src/main/java/com/market/persistance/entity/Categoria.java
@@ -0,0 +1,46 @@
+package com.market.persistance.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String description;
+
+    private Integer estado;
+
+    // Una Categoría tiene muchos productos
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+}
